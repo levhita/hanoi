e9199525677fc940840c0caea5405454d8600f4f
Levhita Arias <levhita@gmail.com>
2023-08-07 20:20:57 -0600

    added a react-create-app for the frontend

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('sdfsdfsdsdfsdf'))
+// Route for React App
+app.use(express.static('./frontend/build/'))
+
+// Route for API
+app.get('/api/', (req, res) => res.send('you are viewing the api'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
